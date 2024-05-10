fee2d5851696d4ded9425c1673830cfd4d2267bd
ZegarkOOO <kondzioklarnet007@gmail.com>
2024-05-10 11:27:55 +0200

    Backend strona logowania + połączenie z DB

--- server.py
+++ server.py
@@ -1,6 +1,27 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import pymysql
 
 app = Flask(__name__)
+
+#----------------------------------------------------------------------------------------------------------------------
+# connection with database
+db = pymysql.connect(
+    charset = "utf8mb4",
+    connect_timeout = 10,
+    cursorclass = pymysql.cursors.DictCursor,
+    db = "bookit_main",
+    host = "bookit-bookit.f.aivencloud.com",
+    password = "AVNS_lK1EnykcZ5J6TflOpru",
+    read_timeout = 10,
+    port = 22474,
+    user = "avnadmin",
+    write_timeout = 10,
+)
+
+# dzięki cursorowi będziemy wysyłać i odbierać zapytania do bazy danych
+cursor = db.cursor()
+
+#------------------------------------------------------------------------------------------------------------------------
 
 # Members API route
 @app.route('/members')
@@ -35,6 +56,46 @@ def get_image_cards():
             }
         ]
     }
+
+
+# dekorator, wpisuje się to na froncie w funkcji fetch() i wtedy jest wywoływana ta funkcja poniżej
+@app.route('/api/strona_logowania/user', methods=['POST']) # ogólnie metoda komuniakcji POST GET się nazywa REST-API podaje dla informacji
+def logging_in():
+    try:
+        # pobranie danych z frontu poprzez JSON
+        login = request.json.get('user_login') # pola podane przez front muszą nazywać się user_login i user_password
+        password = request.json.get('user_password')
+
+        # zwraca listę dobrych dopasowań
+        answer = cursor.execute(f"SELECT * FROM users WHERE email = '{login}' AND password = '{password}';") 
+        # jeśli 
+        if len(answer) > 0:
+            return jsonify({'message': 'Zalogowano pomyślnie!', 'username': login}), 200
+        else:
+            return jsonify({'message': 'Niepoprawne dane logowania!'}), 401
+        
+    except Exception as err:
+        # gdy pojawi się jakiś błąd zwraca error
+        return jsonify({'error': str(err)}), 500
+    
+@app.route('/api/strona_logowania/company', methods=['POST']) # ogólnie metoda komuniakcji POST GET się nazywa REST-API podaje dla informacji
+def logging_in():
+    try:
+        # pobranie danych z frontu poprzez JSON
+        login = request.json.get('company_login') # pola podane przez front muszą nazywać się company_login i company_password
+        password = request.json.get('company_password')
+
+        # zwraca listę dobrych dopasowań
+        answer = cursor.execute(f"SELECT * FROM companies WHERE email = '{login}' AND password = '{password}';") 
+        # jeśli 
+        if len(answer) > 0:
+            return jsonify({'message': 'Zalogowano pomyślnie!', 'username': login}), 200
+        else:
+            return jsonify({'message': 'Niepoprawne dane logowania!'}), 401
+        
+    except Exception as err:
+        # gdy pojawi się jakiś błąd zwraca error
+        return jsonify({'error': str(err)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
