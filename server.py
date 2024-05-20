fdeadbb36414150449d00182741dc72d6c27f51c
Marcin Wiśniowski <marcin.wisniowski@opoczta.pl>
2024-05-21 00:10:04 +0200

    Back do wyszukiwania usług ciąg dalszy

--- server.py
+++ server.py
@@ -8,12 +8,12 @@ app = Flask(__name__)
 # connection with database
 db = pymysql.connect(
     charset = "utf8mb4",
-    connect_timeout = 10,
+    connect_timeout = 100,
     cursorclass = pymysql.cursors.DictCursor,
     db = "bookit_main",
     host = "bookit-bookit.f.aivencloud.com",
     password = "AVNS_lK1EnykcZ5J6TflOpru",
-    read_timeout = 10,
+    read_timeout = 100,
     port = 22474,
     user = "avnadmin",
     write_timeout = 10,
@@ -64,7 +64,38 @@ def get_image_cards():
     except Exception as err:
         # Gdy pojawi się jakiś błąd, zwraca error
         return jsonify({'error': str(err)}), 500
+    
+@app.route('/api/strona_wyszukiwania_kategorie')
+def get_categories():
+    try:
+        cursor.execute(f"SELECT distinct Category FROM companies;")
+        categories = cursor.fetchall()
 
+        result = []
+        for category in categories:
+            print(category)
+            result.append(category['Category'])
+        return jsonify({'categories': result}), 200
+    except Exception as err:
+        print("Błąd zapytania SQL:", str(err))
+        return jsonify({'error': str(err)}), 500
+    
+@app.route('/api/strona_wyszukiwania_miasta')
+def get_cities():
+    try:
+        cursor.execute(f"SELECT distinct City FROM companies;")
+        cities = cursor.fetchall()
+        print(cities)
+        result = []
+        for city in cities:
+            print(city)
+            result.append(city['City'])
+        return jsonify({'cities': result}), 200
+    except Exception as err:
+        print("Błąd zapytania SQL:", str(err))
+        return jsonify({'error': str(err)}), 500
+        
+    
 # dekorator, wpisuje się to na froncie w funkcji fetch() i wtedy jest wywoływana ta funkcja poniżej
 @app.route('/api/strona_logowania/user', methods=['POST']) # ogólnie metoda komuniakcji POST GET się nazywa REST-API podaje dla informacji
 def logging_in_user():
@@ -201,18 +232,18 @@ def return_search():
             kategoria = ''
 
         if sortowanie == 'Najwyższa ocena':
-            cursor.execute(f"SELECT * 
+            cursor.execute(f"""SELECT * 
                             CASE
                                 WHEN Reviews_no > 0 THEN Sum_of_reviews / Reviews_no
                                 ELSE 0
                             END AS srednia_ocena 
                             FROM companies WHERE City = '{miasto}' AND Category = '{kategoria}' 
-                            ORDER BY srednia_ocena DESC ;") 
+                            ORDER BY srednia_ocena DESC ;""") 
             
         elif sortowanie == 'Najpopularniejsze':
-            cursor.execute(f"SELECT * 
+            cursor.execute(f"""SELECT * 
                             FROM companies WHERE City = '{miasto}' AND Category = '{kategoria}' 
-                            ORDER BY Reviews_no DESC ;")
+                            ORDER BY Reviews_no DESC ;""")
         
         companies = cursor.fetchall()
         
@@ -253,11 +284,11 @@ def return_company():
         # pobranie danych z frontu poprzez JSON
         firma = request.json.get('firma')
 
-        cursor.execute(f"SELECT ID, Name, City, Address, Logo, Category,
+        cursor.execute(f"""SELECT ID, Name, City, Address, Logo, Category,
                         Site_link, Facebook_link, Linkedin_link,
                         Instagram_link, X_link, Tiktok_link, Reviews_no,
                         Sum_of_reviews, tel_nr, description 
-                       FROM companies WHERE Name = '{firma}';") 
+                       FROM companies WHERE Name = '{firma}';""") 
                 
         company = cursor.fetchall()
         
