f56a08b98cfc077650c3550abac4731371d7e99e
Wojciech Zajączkowski <zajaczkowski.wojtek003@gmail.com>
2024-06-16 19:49:35 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1149,8 +1149,7 @@ def get_user_reservations():
         return formatted_bookings
 
     try:
-        data = request.json
-        user_email = data.get('User-Email')  # Pobierz email z nagłówków
+        user_email = request.headers.get('User-Email')  # Pobierz email z nagłówków
         if not user_email:
             return jsonify({'error': 'User email is required'}), 400
 
